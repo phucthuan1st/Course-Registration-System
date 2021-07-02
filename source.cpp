caee4a7174ecc92830745657d962e85ff7a66970
phuccr7 <76780540+phuccr7@users.noreply.github.com>
2021-07-02 13:33:24 +0700

    Update source.cpp
    tạo cấu trúc đăng ký

--- source.cpp
+++ source.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <ctime>
+#include<fstream>
+#include <string>
+#include <conio.h>
+#include <Windows.h>
 
+using namespace std;
+//
+struct Time {
+	int hour;
+	int min;
+	int sec;
+};
+//
+struct date {
+	int day;
+	int month;
+	int year;
+	Time time;
+};
+//
+struct registTime {
+	date DayBegin;
+	date DayFinish;
+};
+//
+struct Time_s {
+	string hour;
+	string min;
+	string sec;
+};
+//
+struct date_s {
+	string day;
+	string month;
+	string year;
+	Time_s time;
+};
+//
+struct registTime_s {
+	date_s DayBegin_s;
+	date_s DayFinish_s;
+};
+//
